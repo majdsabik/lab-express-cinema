a91f3e4a6e7082175be9d022d6b32f6c3f2484ec
Majd Sabik <majdsabik.ms@gmail.com>
2020-06-29 16:22:42 +0200

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res) => {
+  Movie.find()
+    .then((moviesFromDatabase) => {
+      // render a 'books' view with the books data
+      console.log(moviesFromDatabase);
+      res.render("movies", { movieList: moviesFromDatabase });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = router;
